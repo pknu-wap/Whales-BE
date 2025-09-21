2cf861ede28d71d6bebfdbafefe658f161ad1906
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-09-21 19:35:04 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/whales/WhalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.whales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whales/WhalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
