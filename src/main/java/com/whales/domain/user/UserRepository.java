374a0e2446d0c475c9328434641edb885dfd97ba
유수환 <yooz7443@naver.com>
2025-09-26 20:12:34 +0900

    refactor: #4 UserRepository 변경

--- src/main/java/com/whales/domain/user/UserRepository.java
+++ src/main/java/com/whales/domain/user/UserRepository.java
@@ -2,11 +2,7 @@ package com.whales.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
