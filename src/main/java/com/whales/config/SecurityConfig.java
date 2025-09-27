cbb4346bb1b35c9c682ccd9e481203cd636661c7
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-09-27 22:41:15 +0900

    chore: SecurityConfig 임시로 모든 접근 허용

--- src/main/java/com/whales/config/SecurityConfig.java
+++ src/main/java/com/whales/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/auth/**"
+                                "/auth/**",
+                                "/**"
                         ).permitAll()
                         // 그 외 인증 필요
                         .anyRequest().authenticated()
