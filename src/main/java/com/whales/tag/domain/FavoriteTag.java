3a001b84391204976a881d06c6f7cbaf23df5a04
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-11-16 01:01:25 +0900

    feat: #10 FavoriteTag 엔티티 추가
    Signed-off-by: GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>

---
+++ src/main/java/com/whales/tag/domain/FavoriteTag.java
@@ -0,0 +1,49 @@
+package com.whales.tag.domain;
+
+import com.whales.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+// 즐겨찾기한 태그 엔티티
+@Entity
+@Table(name = "favorite_tags")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteTag {
+
+    @EmbeddedId
+    private Id id;
+
+    @MapsId("userId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @MapsId("tagId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Tag tag;
+
+    @Builder
+    public FavoriteTag(User user, Tag tag) {
+        this.id = new Id(user.getId(), tag.getId());
+        this.user = user;
+        this.tag = tag;
+    }
+
+    @Embeddable
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Id implements Serializable {
+        @Column(nullable = false)
+        private UUID userId;
+
+        @Column(nullable = false)
+        private UUID tagId;
+    }
+}
