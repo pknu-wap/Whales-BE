18070582e357b149ece13cf4baf30f31ce177056
유수환 <yooz7443@naver.com>
2025-11-22 16:46:24 +0900

    refactor: #4 태그 AND 제목/내용까지 모두 만족하는 검색

--- src/main/java/com/whales/post/domain/PostRepository.java
+++ src/main/java/com/whales/post/domain/PostRepository.java
@@ -10,7 +10,11 @@ import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
+    // UUID로 게시물을 찾되 deletedAt이 NULL인 (삭제되지 않은) 게시물만 찾습니다.
     Optional<Post> findByIdAndDeletedAtIsNull(UUID id);
+
+    // 모든 게시물을 찾되, deletedAt이 NULL인 (삭제되지 않은) 게시물만 찾습니다.
+    //List<Post> findAllByDeletedAtIsNull();
 
     @Query("""
         SELECT p FROM Post p
@@ -20,9 +24,9 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
         GROUP BY p
         HAVING COUNT(DISTINCT t.id) = :tagCount
         """)
-    List<Post> findPostsByAllTagNames(@Param("names") List<String> names,
-                                      @Param("tagCount") Integer tagCount);
+    List<Post> findPostsByAllTagNames(@Param("names") List<String> names, @Param("tagCount") Integer tagCount);
 
+    // LazyLoading(n+1 문제) 방지
     @Query("""
         SELECT DISTINCT p FROM Post p
         LEFT JOIN FETCH p.postTags pt
@@ -32,6 +36,7 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
         """)
     Optional<Post> findByIdWithTagsAndAuthor(@Param("id") UUID id);
 
+    // 제목, 내용으로만 검색
     @Query("""
         SELECT p
         FROM Post p
