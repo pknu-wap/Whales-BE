f972a8d369726fdffce3963617b86913396fe8da
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-11-24 00:43:15 +0900

    feat: #62 UserBadgeColor 생성 Enum 정의(8단계)
    Signed-off-by: GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>

---
+++ src/main/java/com/whales/user/domain/UserBadgeColor.java
@@ -0,0 +1,12 @@
+package com.whales.user.domain;
+
+public enum UserBadgeColor {
+    WHITE,      // 신규 / 기본
+    GRAY,       // 활동중 / 검증 전
+    GREEN,      // 신뢰 회원
+    BLUE,       // 검증된 / 모범 회원
+    PURPLE,     // 상위 기여자 / 우수 멤버
+    GOLD,       // 레전드 / 명예 등급
+    ORANGE,     // 주의 회원
+    RED;        // 경고 회원
+}
