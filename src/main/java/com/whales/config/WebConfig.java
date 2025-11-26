4425bd2242b6ac5feb15cd1e7ebb0641cd008c99
GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>
2025-11-26 21:15:49 +0900

    chore: 프론트 배포 주소로 변경
    Signed-off-by: GPT와 함께라면 두렵지 않아 <isjuny0k@gmail.com>

--- src/main/java/com/whales/config/WebConfig.java
+++ src/main/java/com/whales/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 프론트 주소
+                .allowedOrigins("https://whales-team6.netlify.app") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
